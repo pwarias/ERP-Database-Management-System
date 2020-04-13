d1f5e7e1a811a7c49fb839248aa0a2350a5630e6
Andrew_MacBook <awoltman@hawk.iit.edu>
2020-04-13 10:15:49 -0400

    I updated the file classwork.py

--- classwork.py
+++ classwork.py
@@ -1,44 +1,42 @@
 import psycopg2
-class connection:
-    def __init__():
+class Connection:
+    def __init__(self):
         try:
-    conn = psycopg2.connect(user = "postgres",
-                                  password = "Yaysql37",
-                                  host = "127.0.0.1",
-                                  port = "8080",
-                                  database = "cs425db")
-    cursor = conn.cursor()
+            self.conn = psycopg2.connect(user = "postgres",
+                                        password = "Yaysql37",
+                                        host = "127.0.0.1",
+                                        port = "8080",
+                                        database = "cs425db")
+            self.cursor = self.conn.cursor()
+        except(Exception, psycopg2.Error) as error:
+            print ("Error while connecting to PostgreSQL", error)
 
-    def newUser(usrName,Pasword):
-        return
-    def newCustomer(fn,ln,orderNumber):
-        return
-    def newModel(modelNumber,itemCost,orderNumber):
-        return
-    def newDesign(employeeID,modelNumber,itemCost):
-        return
-    def newEmployee(employeeID,fn,ln,ssNumber,paytype,jobtype):
+    def newUser(self,usrName,Pasword):
         return
 
- 
+    def newCustomer(self,fn,ln,orderNumber):
+        return
+
+    def newModel(self,modelNumber,itemCost,orderNumber):
+        return
+
+    def newDesign(self,employeeID,modelNumber,itemCost):
+        return
+
+    def newEmployee(self,employeeID,fn,ln,ssNumber,paytype,jobtype):
+        return
+
+    def closeConnection(self):
+        if(self.conn):
+            self.cursor.close()
+            self.conn.close()
+            print("PostgeSQL connection is closed")
+
 try:
-    conn = psycopg2.connect(user = "sysadmin",
-                                  password = "",
-                                  host = "127.0.0.1",
-                                  port = "8080",
-                                  database = "postgres_db")
-    cursor = conn.cursor()
-    print(connection.get_dsn_parameters(),"\n")
-
-    cursor.execute("SELECT version();")
-    record = cursor.fetchone()
-    print("You are connected to - ", record, "\n")
-
-except(Exception, psycopg2.Error) as error:
-    print ("Error while connecting to PostgreSQL", error)
+    conTest = Connection()
 
 finally:
-    if(connection):
-        cursor.close()
-        conn.close()
+    if(conTest):
+        conTest.cursor.close()
+        conTest.conn.close()
         print("PostgeSQL connection is closed")
